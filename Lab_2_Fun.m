507480660b0da8501f87db41c6090181aaaf6704
Sarah Li <sarah_li_7@sfu.ca>
2023-03-09 09:16:47 -0800

    GOT NEW GRAPH
    Still have some other errors tho T^T
    but atleast i got a new graph.

--- Lab_2_Fun.m
+++ Lab_2_Fun.m
@@ -1,6 +1,10 @@
-function [z] = Lab_2_Fun(x0,y0);
-x = x0(1);
-y = y0(2);
-z = x^4 + y^4 - 21*x^2 - 13*y^2 + x*y^2 - 14*x - 22*y + 170;
+function [z] = Lab_2_Fun(x1,y1);
+s = size(x1);
+for i = 1:s
+    x = x1(i);
+    y = y1(i);
+    f = (x^2 + y - 11)^2 + (x + y^2 - 7)^2;
+    z(i) = f;
+end 
 
     
